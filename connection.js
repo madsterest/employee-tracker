f7a4526ae22c7cda0ba1188fc8698f1fc20aae3c
madsterest <odea.madeleine@gmail.com>
2021-05-23 02:00:34 +1000

    Continued working on prompt questions. Fixed choices function. Removed functions folder

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("dotenv").config();
 
-const readFunction = require("./function/read");
+// const readFunction = require("./function/read");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -32,24 +32,101 @@ const init = () => {
     ])
     .then((data) => {
       if (data.select === "Add New Department") {
-        //create function(Department)
+        newDep();
       } else if (data.select === "Add New Role") {
         //create function(Role)
       } else if (data.select === "Add New Employee") {
-        //create function(Employee)
+        newEmploy();
       } else if (data.select === "View Departments") {
-        readFunction(department);
+        let tableName = "department";
+        viewTable(tableName);
       } else if (data.select === "View Roles") {
-        readFunction(role);
+        let tableName = "role";
+        viewTable(tableName);
       } else if (data.select === "View Employees") {
-        readFunction(employee);
+        let tableName = "employee";
+        viewTable(tableName);
       } else if (data.select === "Update Employee Roles") {
         //updated function(Employees)
       }
     });
 };
 
+const viewTable = (tableName) => {
+  connection.query(`SELECT * FROM ${tableName}`, (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    init();
+  });
+};
+
+const newDep = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "depName",
+        message: "What is the department name?",
+      },
+    ])
+    .then((input) => {
+      newDepQuery(input);
+    });
+};
+
+const readRole = () => {
+  connection.query(`SELECT title FROM role`, (err, results) => {
+    if (err) throw err;
+    const choiceArray = [];
+    results.forEach(({ roleTitle }) => {
+      choiceArray.push(roleTitle);
+      return choiceArray;
+    });
+    newEmploy(choiceArray);
+  });
+};
+
+const newEmploy = () => {
+  connection.query(`SELECT title FROM role`, (err, results) => {
+    if (err) throw err;
+    console.log(results, typeof results);
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices() {
+            let choiceArray = [];
+            results.forEach((roleTitle) => {
+              choiceArray.push(roleTitle.title);
+            });
+            return choiceArray;
+          },
+          message: "What is the employees role?",
+        },
+      ])
+      .then((input) => {
+        console.log(input);
+      });
+  });
+};
+
+const newDepQuery = (input) => {
+  connection
+    .query(`INSERT INTO department SET ?`, {
+      name: input.depName,
+    })
+    .then((err, res) => {
+      if (err) throw err;
+      console.log(res);
+      init();
+    });
+};
+
+const newEmployQuery = () => {};
+
 connection.connect((err) => {
   if (err) throw err;
   console.log(`Connected at ${connection.threadId}`);
+  init();
 });
--- function/read.js
+++
@@ -1,9 +0,0 @@
-const viewTable = (tableName) => {
-  console.log(`${tableName}\n`);
-  connection.query(`SELECT * FROM ${tableName}`, (err, res) => {
-    if (err) throw err;
-    init();
-  });
-};
-
-module.exports = viewTable;
